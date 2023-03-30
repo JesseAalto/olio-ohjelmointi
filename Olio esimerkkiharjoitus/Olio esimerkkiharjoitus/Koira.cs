21190749319d74636c1edcb545fdd2805031a4d1
JesseAalto <jesse.aalto@student.careeria.fi>
2023-03-30 13:52:22 +0300

    Koiraolio

---
+++ Olio esimerkkiharjoitus/Olio esimerkkiharjoitus/Koira.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_esimerkkiharjoitus
+{
+    internal class Koira
+    {
+        // koira -olion muuttuja
+
+        public string Nimi { get; set; }
+
+        public string rotu { get; set; }
+
+        public int ikä { get; set; }
+
+        public String Väri { get; set; }
+
+        public float paino { get; set; }
+
+
+        public Koira(string _nimi, string _rotu, int _ikä, string _väri, float _paino)
+        {
+            Nimi = _nimi;
+            rotu = _rotu;
+            ikä = _ikä;
+            Väri = _väri;
+            paino = _paino;
+        }
+
+
+        public string HaeTiedot()
+        {
+            string Koirantiedot = "Nimi: " + Nimi + ". Rotu: " + rotu + ". Ikä: " + ikä.ToString() + ". Väri: " + Väri + ". Paino: " + paino.ToString();
+
+            return Koirantiedot;
+
+
+        }
+        public void Hauku()
+        {
+            Console.WriteLine("Woof Woof");
+        }
+
+    }           
+
+
+}
---
+++ Olio esimerkkiharjoitus/Olio esimerkkiharjoitus/Program.cs
@@ -0,0 +1,23 @@
+﻿using Olio_esimerkkiharjoitus;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Koira KoiraOlio = new Koira("Rekku", "Saksanpaimenkoira", 4, "Harmaa", 32.0f);
+
+        KoiraOlio.Nimi =  "Rekku";
+        KoiraOlio.rotu = "Saksanpaimenkoira";
+        KoiraOlio.ikä = 4;
+        KoiraOlio.Väri = "Ruskea";
+        KoiraOlio.paino = 32.0f;
+
+        KoiraOlio.Hauku();
+
+        Console.WriteLine(KoiraOlio.HaeTiedot());
+
+        KoiraOlio.Hauku();
+
+    }
+
+}
