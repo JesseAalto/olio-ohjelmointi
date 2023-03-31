8ecb49556f82211b81dea3afde82982ab7179ede
JesseAalto <jesse.aalto@student.careeria.fi>
2023-03-31 10:48:17 +0300

    Autoharjoitus

--- Harjoituss1/Harjoituss1/Program.cs
+++ Harjoituss1/Harjoituss1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Harjoituss1; 
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        auto AutoOlio = new auto();
+        
+        AutoOlio.Nimi = "Maatti ";
+        AutoOlio.Nopeus = 100;
+        AutoOlio.Renkaat =  4;
+
+        Console.WriteLine(AutoOlio.TulostaData());
+    }
+}
---
+++ Harjoituss1/Harjoituss1/auto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoituss1
+{
+    internal class auto
+    {
+        public string Nimi { get; set; }
+
+        public int Nopeus { get; set; }
+
+        public int Renkaat { get; set; }
+
+        // Tällä metodilla noudatetaan olemassa olevaa auto- olion tietoja
+
+        public string TulostaData()
+        {
+            string AutonTiedot = "Nimi: " + Nimi + "Nopeus: " + Nopeus.ToString() + " Renkaat: " + Renkaat.ToString();
+
+            return AutonTiedot;
+
+
+
+        }
+
+
+
+
+
+    }
+}
