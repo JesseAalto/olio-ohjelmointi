8c1440667470096c9a46a24b5dc457212bbd6837
JesseAalto <jesse.aalto@student.careeria.fi>
2023-04-06 09:22:33 +0300

    Opiskelija harjoitus2.cs

---
+++ Opiskelija harjoitus2/Opiskelija harjoitus2/OpiskelijaHarjoitus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opiskelija_harjoitus2
+{
+    internal class Opiskelija
+    {
+        // Opiskelija -olion muuttuja
+
+        public string Nimi { get; set; }
+
+        public string OpiskelijaID { get; set; }
+
+        public int Opintopisteet { get; set; }
+
+        
+
+
+        public Opiskelija(string _nimi, string _OpiskelijaID, int _Opintopisteet)
+        {
+            Nimi = _nimi;
+            OpiskelijaID = _OpiskelijaID;
+            Opintopisteet = _Opintopisteet;
+           
+        }
+
+
+        public string HaeTiedot()
+        {
+            string Opiskelijatiedot = "Nimi: " + Nimi + ". OpiskelijaID: " + OpiskelijaID + ". Opintopisteet: " + Opintopisteet.ToString();
+
+            return Opiskelijatiedot;
+
+
+        }
+        public int MuokkaaOpintopisteet()
+        {
+            Opintopisteet = 2;
+
+            return Opintopisteet; 
+        }
+
+    }
+
+
+}
