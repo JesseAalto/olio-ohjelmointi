eb628caa6800d3ec73b9b95c21e66e36f63a1cd4
Jesse Aalto <jesseaalto7@gmail.com>
2023-04-26 10:48:53 +0300

    L

---
+++ Laskija/Laskija/Program.cs
@@ -0,0 +1,20 @@
+﻿using Laskija;
+
+namespace Laskija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 25.0f;
+            float b = 10.5f;
+
+            float summa = Laskin.Summa(a, b);
+            float erotus = Laskin.Erotus(a, b);
+            float kertolasku = Laskin.Kertolasku(a, b);
+            float jakolasku = Laskin.Jakolasku(a, b);
+
+            Console.WriteLine("Numeroiden {0} ja {1}:", a, b + "\nSumma: " + summa + "\nErotus: " + erotus + "\nKertolasku: " + kertolasku + "\nJakolasku: " + jakolasku);
+        }
+    }
+}
