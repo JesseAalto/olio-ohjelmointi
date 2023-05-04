f305fed5e33bbdaa1b8562283c0fc519a126f87e
Jesse Aalto <jesseaalto7@gmail.com>
2023-05-04 12:48:46 +0300

    r

---
+++ Harjoitus_12/Harjoitus_12/Program.cs
@@ -0,0 +1,12 @@
+﻿using Harjoitus_12;
+class Ohjelma
+{
+    static void Main()
+    {
+        Korttipakka pakka = new Korttipakka();
+        pakka.TulostaKortit();
+        pakka.SekoitaKortit();
+        Console.WriteLine("\nKortit on sekoitettu:");
+        pakka.TulostaKortit();
+    }
+}
