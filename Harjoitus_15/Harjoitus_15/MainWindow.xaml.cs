e60c5da757ed172f4ae09ae4b896b61a3a3ae6d2
Jesse Aalto <jesseaalto7@gmail.com>
2023-05-05 09:20:21 +0300

    r

---
+++ Harjoitus_15/Harjoitus_15/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus_15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int HenkilöAutot = 0;
+        private int KuormaAutot = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            tbHenkilöauto1.Text = HenkilöAutot.ToString();
+            tbKuormaauto1.Text = KuormaAutot.ToString();
+        }
+        private void BtnHenkilöAuto_Click(object sender, RoutedEventArgs e)
+        {
+             HenkilöAutot++;
+            tbHenkilöauto1.Text = HenkilöAutot.ToString();
+        }
+
+        private void BtnKuormaAuto_Click_1(object sender, RoutedEventArgs e)
+        {
+             KuormaAutot++;
+            tbKuormaauto1.Text = KuormaAutot.ToString();
+        }
+    }
+}
